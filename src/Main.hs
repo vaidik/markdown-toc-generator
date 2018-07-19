4b295672660a9c67937e87804fe889e0c4b18b65
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2018-07-19 13:50:11 +0530

    generate ordered list of headings

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,10 @@
 
 module Main where
 
-import CMark --(commonmarkToNode, Node, NodeType)
+import CMark
 import Data.Text (pack, unpack)
 import Data.Text.Internal (Text)
+import Data.Maybe (fromJust)
 
 get_type :: Node -> NodeType
 get_type (Node _ t _) = t
@@ -23,11 +24,6 @@ get_level nt = error ("NodeType " ++ (show nt) ++ " is not HEADING type")
 filter_headings :: [Node] -> [Node]
 filter_headings n = filter (\x -> (is_heading (get_type x)) == True) n
 
--- slice :: [Int] -> Int -> [Int]
--- slice [] _ = []
--- slice [x] l = if x > l then x else []
--- slice (x:xs) = if x > l then x:(slice xs l) else 
-
 restructure_nodes :: [Int] -> Int -> [Int]
 restructure_nodes [] _ = []
 restructure_nodes [x] l = if x > l then [x] else []
@@ -38,21 +34,40 @@ restructure_nodes (x:xs) l
 get_text :: Node -> Text
 get_text (Node _ (TEXT s) _) = s
 
-make_item :: Node -> Node
-make_item (Node _ t c) = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (get_text (c !! 0))) []]]
+make_list :: [Node] -> Node
+make_list n = Node Nothing (LIST (ListAttributes {listType = ORDERED_LIST, listTight = True, listStart = 1, listDelim = PERIOD_DELIM})) (map (\x -> make_node x Nothing) n)
+
+make_node :: Node -> Maybe Node -> Node
+make_node (Node _ t c) Nothing = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (get_text (c !! 0))) []]]
+make_node (Node _ t c) n = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (get_text (c !! 0))) []], (fromJust n)]
+
+slice :: [Node] -> Int -> [Node]
+slice [] l = []
+slice [x] l = if (get_level (get_type x)) > l then [x] else []
+slice (x:xs) l
+  | level_x > l   = x:(slice xs l)
+  | level_x <= l  = []
+  where level_x = get_level (get_type x)
+
+reverse_slice :: [Node] -> Int -> [Node]
+reverse_slice x l = drop (length (slice x l)) x
+
+make_list' :: [Node] -> [Node]
+make_list' [] = []
+make_list' [x] = [make_node x Nothing]
+make_list' (x:y:xs)
+  | level_x == level_y  = (make_node x Nothing):(make_list' (y:xs))
+  | level_x < level_y   = (make_node x (Just (make_list_node (make_list' (slice (y:xs) level_x))))):(make_list' (reverse_slice (y:xs) level_x))
+  | level_x > level_y   = error "Unexpected condition"
+    where level_x = get_level (get_type x)
+          level_y = get_level (get_type y)
+
+make_list_node :: [Node] -> Node
+make_list_node n = Node Nothing (LIST (ListAttributes {listType = ORDERED_LIST, listTight = True, listStart = 1, listDelim = PERIOD_DELIM})) n
 
 main :: IO ()
 main = do
   md <- readFile "README.test.md"
-  result <- readFile "README.result.md"
-  let md_nodes = commonmarkToNode [] (pack md)
-  putStrLn (show md_nodes)
-  putStrLn md
-  putStrLn "\n"
-  putStrLn (show (get_children (commonmarkToNode [] (pack result))))
-  let levels = (map (\x -> get_level (get_type x)) (filter_headings (get_children md_nodes)))
-  putStrLn (show levels)
-  putStrLn (show (restructure_nodes levels 1))
-  putStrLn (show (restructure_nodes levels 2))
-
-  putStrLn (show (make_item ((get_children md_nodes) !! 0)))
+  let md_nodes = get_children (commonmarkToNode [] (pack md))
+  let levels = (map (\x -> get_level (get_type x)) (filter_headings md_nodes))
+  putStrLn (unpack (nodeToCommonmark [] Nothing (make_list_node (make_list' (filter_headings md_nodes)))))
