d8375654030962f201261fd095ae7bad5107e118
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2018-07-20 07:57:27 +0530

    fix hlint issues

--- src/Main.hs
+++ src/Main.hs
@@ -7,67 +7,59 @@ import Data.Text (pack, unpack)
 import Data.Text.Internal (Text)
 import Data.Maybe (fromJust)
 
-get_type :: Node -> NodeType
-get_type (Node _ t _) = t
+getType :: Node -> NodeType
+getType (Node _ t _) = t
 
-get_children :: Node -> [Node]
-get_children (Node _ _ n) = n
+getChildren :: Node -> [Node]
+getChildren (Node _ _ n) = n
 
-is_heading :: NodeType -> Bool
-is_heading (HEADING _) = True
-is_heading _ = False
+isHeading :: NodeType -> Bool
+isHeading (HEADING _) = True
+isHeading _ = False
 
-get_level :: NodeType -> Int
-get_level (HEADING l) = l
-get_level nt = error ("NodeType " ++ (show nt) ++ " is not HEADING type")
+getLevel :: NodeType -> Int
+getLevel (HEADING l) = l
+getLevel nt = error ("NodeType " ++ show nt ++ " is not HEADING type")
 
-filter_headings :: [Node] -> [Node]
-filter_headings n = filter (\x -> (is_heading (get_type x)) == True) n
+filterHeadings :: [Node] -> [Node]
+filterHeadings = filter $ isHeading . getType
 
-restructure_nodes :: [Int] -> Int -> [Int]
-restructure_nodes [] _ = []
-restructure_nodes [x] l = if x > l then [x] else []
-restructure_nodes (x:xs) l
-  | x > l = x:(restructure_nodes xs x)
-  | x <= l = restructure_nodes xs l
+getText :: Node -> Text
+getText (Node _ (TEXT s) _) = s
 
-get_text :: Node -> Text
-get_text (Node _ (TEXT s) _) = s
+makeList :: [Node] -> Node
+makeList n = Node Nothing (LIST (ListAttributes {listType = ORDERED_LIST, listTight = True, listStart = 1, listDelim = PERIOD_DELIM})) (map (`makeNode` Nothing) n)
 
-make_list :: [Node] -> Node
-make_list n = Node Nothing (LIST (ListAttributes {listType = ORDERED_LIST, listTight = True, listStart = 1, listDelim = PERIOD_DELIM})) (map (\x -> make_node x Nothing) n)
-
-make_node :: Node -> Maybe Node -> Node
-make_node (Node _ t c) Nothing = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (get_text (c !! 0))) []]]
-make_node (Node _ t c) n = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (get_text (c !! 0))) []], (fromJust n)]
+makeNode :: Node -> Maybe Node -> Node
+makeNode (Node _ t c) Nothing = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (getText (head c))) []]]
+makeNode (Node _ t c) n = Node Nothing ITEM [Node Nothing PARAGRAPH [Node Nothing (TEXT (getText (head c))) []], fromJust n]
 
 slice :: [Node] -> Int -> [Node]
 slice [] l = []
-slice [x] l = if (get_level (get_type x)) > l then [x] else []
+slice [x] l = [x | getLevel (getType x) > l]
 slice (x:xs) l
-  | level_x > l   = x:(slice xs l)
+  | level_x > l   = x : slice xs l
   | level_x <= l  = []
-  where level_x = get_level (get_type x)
+  where level_x = getLevel (getType x)
 
-reverse_slice :: [Node] -> Int -> [Node]
-reverse_slice x l = drop (length (slice x l)) x
+reverseSlice :: [Node] -> Int -> [Node]
+reverseSlice x l = drop (length (slice x l)) x
 
-make_list' :: [Node] -> [Node]
-make_list' [] = []
-make_list' [x] = [make_node x Nothing]
-make_list' (x:y:xs)
-  | level_x == level_y  = (make_node x Nothing):(make_list' (y:xs))
-  | level_x < level_y   = (make_node x (Just (make_list_node (make_list' (slice (y:xs) level_x))))):(make_list' (reverse_slice (y:xs) level_x))
+makeList' :: [Node] -> [Node]
+makeList' [] = []
+makeList' [x] = [makeNode x Nothing]
+makeList' (x:y:xs)
+  | level_x == level_y  = makeNode x Nothing : makeList' (y:xs)
+  | level_x < level_y   = makeNode x (Just (makeListNode (makeList' (slice (y : xs) level_x)))) : makeList' (reverseSlice (y : xs) level_x)
   | level_x > level_y   = error "Unexpected condition"
-    where level_x = get_level (get_type x)
-          level_y = get_level (get_type y)
+    where level_x = getLevel (getType x)
+          level_y = getLevel (getType y)
 
-make_list_node :: [Node] -> Node
-make_list_node n = Node Nothing (LIST (ListAttributes {listType = ORDERED_LIST, listTight = True, listStart = 1, listDelim = PERIOD_DELIM})) n
+makeListNode :: [Node] -> Node
+makeListNode = Node Nothing (LIST (ListAttributes {listType = ORDERED_LIST, listTight = True, listStart = 1, listDelim = PERIOD_DELIM}))
 
 main :: IO ()
 main = do
   md <- readFile "README.test.md"
-  let md_nodes = get_children (commonmarkToNode [] (pack md))
-  let levels = (map (\x -> get_level (get_type x)) (filter_headings md_nodes))
-  putStrLn (unpack (nodeToCommonmark [] Nothing (make_list_node (make_list' (filter_headings md_nodes)))))
+  let mdNodes = getChildren (commonmarkToNode [] (pack md))
+  putStrLn (unpack (nodeToCommonmark [] Nothing (makeListNode (makeList' (filterHeadings mdNodes)))))
